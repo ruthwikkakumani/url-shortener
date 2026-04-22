245c7d416356a051391b2aa801ecfeaf221e4cc1
RuthwikKakumani <ruthwikkakumani08@gmail.com>
2026-04-23 03:56:09 +0530

    fix(model): hide password field from JSON responses

--- services/auth-service/internal/model/user.go
+++ services/auth-service/internal/model/user.go
@@ -4,5 +4,5 @@ type User struct {
 	ID string `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
 	Email string `json:"email" db:"email"`
-	Password string `json:"password" db:"password"`
+	Password string `json:"-" db:"password"`
 }
